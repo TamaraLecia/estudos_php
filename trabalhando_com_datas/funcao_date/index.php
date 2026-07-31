c8f723dabde5c5915e6b8eea09c94958b2b351f8
Tamara Lécia <tamaralecia976@gmail.com>
2026-07-31 14:12:33 -0300

    Trabalhando com datas em php

---
+++ trabalhando_com_datas/funcao_date/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $d = date('d/m/y'); //day /month / year
+
+	echo $d . "<br>";
+
+	$d2 = date('d, M - Y'); // dia / Abreviatura do mês / ano completo
+
+	echo $d2 . "<br>";
+
+	$d3 = date('d/m/Y'); // dia / mês em número / ano completo
+
+	echo $d3 . "<br>";
+
+	$d4 = date('I, F - Y'); // dia da semana / nome do mês / ano completo
+
+	echo $d4 . "<br>";
+
+
+?>
